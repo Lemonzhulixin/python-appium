277dcf6db47926007659ff9dd103b8c23dc2a4b4
zhulixin <lixin.zhu@quvideo.com>
2018-09-14 10:11:03 +0800

    0914

--- TestCase/galleryTest.py
+++ TestCase/galleryTest.py
@@ -23,7 +23,7 @@ class GalleryTest(ParametrizedTestCase):
             ReplaceYaml(tc, tc_temp, el_iOS)
 
     def test_gallery_img(self):
-        tc = PATH("../yamls/test_home/test_gallery_img.yaml")
+        tc = PATH("../yamls/test_gallery/test_gallery_img.yaml")
         self.repalce(tc, tc_temp)
 
         app = {"logTest": self.logTest, "driver": self.driver, "path": tc_temp,
@@ -34,7 +34,7 @@ class GalleryTest(ParametrizedTestCase):
         page.checkPoint()
 
     def test_gallery_video(self):
-        tc = PATH("../yamls/test_home/test_gallery_video.yaml")
+        tc = PATH("../yamls/test_gallery/test_gallery_video.yaml")
         self.repalce(tc, tc_temp)
 
         app = {"logTest": self.logTest, "driver": self.driver, "path": tc_temp,
