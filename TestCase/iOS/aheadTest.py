9be016c6e5df53b48ed1b5c096dcdedfcc75bb67
zhulixin <lixin.zhu@quvideo.com>
2018-09-27 17:27:33 +0800

    modify TCs

---
+++ TestCase/iOS/aheadTest.py
@@ -0,0 +1,50 @@
+from Base.BaseRunner import ParametrizedTestCase
+import os
+import sys
+from PageObject.Operate.PageOperate import PageOperate
+from Base.BaseReplace import ReplaceYaml
+
+
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+
+tc_temp = PATH("../yamls/temp.yaml")
+el_android = PATH("../yamls/el_android.yaml")
+el_iOS = PATH("../yamls/el_iOS.yaml")
+
+class PrivacySet(ParametrizedTestCase):
+
+    def repalce(self, tc, tc_temp):
+        if self.platformName == 'android':
+            ReplaceYaml(tc, tc_temp, el_android)
+        elif self.platformName == 'iOS':
+            ReplaceYaml(tc, tc_temp, el_iOS)
+
+    def test_privacy(self):
+        tc = PATH("../yamls/iOS/test_ahead/test_privacy.yaml")
+        self.repalce(tc, tc_temp)
+        app = {"logTest": self.logTest, "driver": self.driver, "path": tc_temp,
+               "device": self.udid, "platformName": self.platformName, "caseName": sys._getframe().f_code.co_name}
+
+        page = PageOperate(app)
+        page.operate()
+        page.checkPoint()
+
+    def test_login(self):
+        tc = PATH("../yamls/iOS/test_ahead/test_login.yaml")
+        self.repalce(tc, tc_temp)
+        app = {"logTest": self.logTest, "driver": self.driver, "path": tc_temp,
+               "device": self.udid, "platformName": self.platformName, "caseName": sys._getframe().f_code.co_name}
+
+        page = PageOperate(app)
+        page.operate()
+        page.checkPoint()
+
+    @classmethod
+    def setUpClass(cls):
+        super(PrivacySet, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(PrivacySet, cls).tearDownClass()
